86d362d51a27f69a8cd071854ec7d3f91af0f579
Scott G <scottg489@gmail.com>
2016-12-10 23:22:32 -0500

    Make deadref multi-threaded
    Spin up a new thread to test each reference

--- deadref.py
+++ deadref.py
@@ -5,23 +5,35 @@ import urllib2
 import urllib
 import sys
 
+import threading
+import ssl
+
 def main():
+    # TODO: Is opener thread safe?
     opener = urllib2.build_opener()
-    #opener.addheaders = [('User-agent', 'Mozilla/5.0')]
+
     opener.addheaders = [('User-agent', 'igoturuseragentrighthere')]
-    #response = opener.open('http://en.wikipedia.org/wiki/Big_Table')
+
     response = opener.open('http://en.wikipedia.org/wiki/' + sys.argv[1])
     html_file = open('wiki.html', 'w')
     html_file.write(response.read())
     html_file.close()
     root = ET.parse('wiki.html').getroot()
-    #print root.findall("*[@class='references']")
+
     for link in root.findall(".//*[@class='references'].//*a"):
         if link.get('href').startswith('http'):
-            try:
-                opener.open(link.get('href'))
-            except urllib2.URLError as e:
-                print link.get('href')
+            test_ref(opener, link)
+
+def test_ref(opener, link):
+    t = threading.Thread(target=do_test, args=(opener, link,))
+    t.start()
+
+def do_test(opener, link):
+    try:
+        opener.open(link.get('href'), timeout=5)
+    except Exception as error:
+        #print link.get('href') + ": " + str(error)
+        print link.get('href')
 
 if __name__ == '__main__':
     main()
